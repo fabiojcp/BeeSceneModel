e16039e193289882bf43954388000d7e956e9204
Fábio Casanova <fabio50019@gmail.com>
2024-03-23 11:17:40 -0300

    feat: camera perspective from mouse move

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,67 @@
-import { Suspense } from 'react'
-import * as THREE from 'three'
-import { Canvas, useLoader } from '@react-three/fiber'
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js'
-import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js'
-import { Environment, OrbitControls } from '@react-three/drei'
-import { DDSLoader } from 'three-stdlib'
+import { Suspense, useRef } from "react";
+import * as THREE from "three";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
+import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader.js";
+import { Environment, Preload } from "@react-three/drei";
+import { DDSLoader } from "three-stdlib";
 
-THREE.DefaultLoadingManager.addHandler(/\.dds$/i, new DDSLoader())
+THREE.DefaultLoadingManager.addHandler(/\.dds$/i, new DDSLoader());
 
 function Bee() {
-  const materials = useLoader(MTLLoader, './Bee.mtl')
-  const obj = useLoader(OBJLoader, './Bee.obj', (loader) => {
-    materials.preload()
-    loader.setMaterials(materials)
-  })
+  const ref = useRef(null);
+  const materials = useLoader(MTLLoader, "./Bee.mtl");
+  const obj = useLoader(OBJLoader, "./Bee.obj", (loader) => {
+    materials.preload();
+    loader.setMaterials(materials);
+  });
 
-  return <primitive object={obj} />
+  return <primitive object={obj} ref={ref} />;
 }
+
+function Rig() {
+  useFrame((state) => {
+    // console.log(state.camera.position);
+    // _Vector3{x: 0, y: 0, z: 5}
+    state.camera.position.lerp(
+      { x: -state.pointer.x * 2, y: -state.pointer.y * 2, z: 5 },
+      0.1
+    );
+    state.camera.lookAt(-1, 0, 0);
+  });
+}
+
 function App() {
   return (
     <div
       style={{
-        width: '100vw',
-        height: '100vh',
-        background: 'black',
+        width: "100vw",
+        height: "100vh",
+        background: "black",
         margin: 0,
         padding: 0,
-        position: 'absolute',
+        position: "absolute",
         top: 0,
         left: 0,
       }}
     >
-      <Canvas>
+      <Canvas camera={{ fov: 80 }}>
         <Suspense fallback={null}>
           <ambientLight intensity={1} />
           <directionalLight color="red" position={[0, 0, 5]} />
           <Bee />
-          <OrbitControls />
+          {/* <PerspectiveCamera fov={500} /> */}
+          {/* <OrbitControls
+            minPolarAngle={Math.PI / 2.5}
+            maxPolarAngle={Math.PI / 2.5}
+          /> */}
           <Environment preset="sunset" background />
+          <Rig from={-Math.PI / 2} to={Math.PI / 2.66} />
+          <Preload all />
         </Suspense>
       </Canvas>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
